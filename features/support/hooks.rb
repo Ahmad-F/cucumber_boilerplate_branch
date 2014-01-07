d1f90e7e89f622f267dff49dbad0b58cc03dda5d
Scott Clark <sclark@chathamfinancial.com>
2014-01-07 14:36:47 -0500

    Fix screenshot and browser closing bugs, gitignore pngs

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,14 +1,18 @@
 require "watir-webdriver"
 
 Before do
-  @browser = Watir::Browser.new :firefox
+  debug_profile = Selenium::WebDriver::Firefox::Profile.new
+  debug_profile["network.automatic-ntlm-auth.trusted-uris"] = "extranetdev.chathamfinancial.com"
+
+  @browser = Watir::Browser.new(:firefox, :profile => debug_profile)
 end
 
 After do |scenario|
   if scenario.failed?
-    filename = "error-#{@current_page.class}-#{Time.now}.png"
+    time_stamp = Time.now.strftime("%Y_%m_%d_%H_%M")
+    filename = "error-#{@current_page.class}-#{time_stamp}.png"
     @current_page.save_screenshot(filename)
-    embed(filename, "image/png")
+    embed(filename, 'image/png')
   end
 
   @browser.close
